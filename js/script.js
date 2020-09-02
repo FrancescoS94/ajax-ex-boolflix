6048693357e08abdcd936b1f4225f878e8397de4
FrancescoS <francesco.salerno2294@gmail.com>
2020-09-02 15:29:45 +0200

    Work2

--- js/script.js
+++ js/script.js
@@ -11,37 +11,44 @@
 // 4 - Voto.
 
 $(document).ready(function() {
-    $("#btn-click").click(function() {
-        var ricerca = $("#input").val();
-        $(".lista-film").empty();
-        $.ajax(
-            {
-                url: "https://api.themoviedb.org/3/search/movie",
-                method: "GET",
-                data: {
-                    api_key: "96df83a2ec59d64214d9e5a29ad76f29",
-                    query: ricerca,
-                    language: "it-IT"
-                },
-                success: function(risposta) {
-                    for (var i = 0; i < risposta.results.length; i++) {
-                        var source = $("#film-template").html();
-                        var template = Handlebars.compile(source);
 
-                        var context = {
-                            title: risposta.results[i].title,
-                            original_title: risposta.results[i].original_title,
-                            original_language: risposta.results[i].original_language,
-                            vote_average: risposta.results[i].vote_average
-                        };
-                        var html = template(context);
-                        $(".lista-film").append(html);
-                    };
-                },
-                error: function() {
-                    alert("Errore");
-                }
-            }
-        );
+    $("#btn-click").click(function() {
+        ricercaFilm();
     });
 });
+
+// ** FUNZIONI **
+
+function ricercaFilm() {
+    var ricerca = $("#input").val();
+    $(".lista-film").empty();
+    $.ajax(
+        {
+            url: "https://api.themoviedb.org/3/search/movie",
+            method: "GET",
+            data: {
+                api_key: "96df83a2ec59d64214d9e5a29ad76f29",
+                query: ricerca,
+                language: "it-IT"
+            },
+            success: function(risposta) {
+                for (var i = 0; i < risposta.results.length; i++) {
+                    var source = $("#film-template").html();
+                    var template = Handlebars.compile(source);
+
+                    var context = {
+                        title: risposta.results[i].title,
+                        original_title: risposta.results[i].original_title,
+                        original_language: risposta.results[i].original_language,
+                        vote_average: risposta.results[i].vote_average
+                    };
+                    var html = template(context);
+                    $(".lista-film").append(html);
+                };
+            },
+            error: function() {
+                alert("Errore");
+            }
+        }
+    );
+};
